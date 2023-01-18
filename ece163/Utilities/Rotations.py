fe3ad3605ddfe059ce48996429817617e207dadb
Robert Gaines <regaines@ucsc.edu>
2023-01-17 18:38:42 -0800

    added three base rotation functions they still need to be tested.

--- ece163/Utilities/Rotations.py
+++ ece163/Utilities/Rotations.py
@@ -1,2 +1,64 @@
+"""
+Engineer: Robert Gaines
+File Name : Rotations.py
+Purpose : Contains functions to compute Rotation Matrix Math for use in a flight sim
+"""
 import math
 from . import MatrixMath
+
+
+def dcm2Euler(DCM):
+    yaw = pitch = roll = 0
+    #returns yaw,pitch,roll in radians
+
+    roll = math.sinh(DCM[0][2])  #check for +-1 bounds
+    pitch = math.atan2(DCM[1][2], DCM[2][2])
+    yaw = math.atan2(DCM[0][1], DCM[0][0])
+    a = [yaw, pitch, roll]
+
+    return a
+
+
+def euler2DCM(yaw, pitch, roll):
+
+    col=4
+    rows=3
+    matrix=[[0]*col for i in range(rows)]
+    #first row of DCM
+    matrix[0][0] = math.cos(roll) * math.cos(yaw)
+    matrix[0][1] = math.cos(roll) * math.sin(yaw)
+    matrix[0][2] = -1 * math.sin(roll)
+
+    #second row
+    matrix[1][0] = math.sin(pitch)*math.sin(roll)*math.cos(yaw) - math.cos(roll)*math.sin(yaw)
+    matrix[1][1] = math.sin(pitch)*math.sin(roll)*math.sin(yaw) + math.cos(pitch)*math.cos(yaw)
+    matrix[1][2] = math.sin(pitch)*math.cos(roll)
+
+    #third row
+    matrix[2][0] = math.cos(pitch)*math.sin(roll)*math.cos(yaw) + math.sin(pitch)*math.sin(yaw)
+    matrix[2][1] = math.cos(pitch)*math.sin(roll)*math.sin(yaw) - math.sin(pitch)*math.cos(yaw)
+    matrix[2][2] = math.cos(pitch)*math.cos(roll)
+
+    return matrix;
+
+
+#points is a Nx3 matrix of points (x,y,z);
+#function will take xyz coordinates in a NED frame and convert them for the ENU frame
+
+# Pned = [North,
+#         East
+#         Down]
+
+#Penu = [East
+#        North
+#        Up]
+def ned2enu(points):
+    #basically x,y coordinates are switched in the vector, and the z coordinate is negated.
+    temp = 0
+    for point in points:
+        temp = point[0]
+        point[0] = point[1]
+        point[1] = temp
+        point[2] = point[2] * -1
+
+    return points
