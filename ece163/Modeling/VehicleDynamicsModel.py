50ae013012b0ff0bcfb372ceeec064ad55bd8b0c
Robert Gaines <regaines@ucsc.edu>
2023-01-26 10:36:31 -0800

    added all the functions i believe

--- ece163/Modeling/VehicleDynamicsModel.py
+++ ece163/Modeling/VehicleDynamicsModel.py
@@ -8,7 +8,7 @@ class VehicleDynamicsModel:
     
     def __init__(self, dT = VPC.dT):
         self.state = States.vehicleState()
-        self.dot = 0
+        self.dot = States.vehicleState()
         self.dT = dT
 
     def ForwardEuler(self, dT, state, dot):
@@ -23,8 +23,36 @@ class VehicleDynamicsModel:
         rexp = 0
         return rexp
 
-    def Update(forcesMoments):
+    def Update(self,forcesnmoments):
 
         print("start of update")
 
         print("end of update.")
+
+    def derivative(self,state, forcesnmoments):
+        #compute the derivative of the given state with the provided forces n moments then return 
+        #an updated state where the derivatives replace what they dervied from i.e pqr => PdotQdotRdot
+
+        return state
+
+    def getVehicleDerivative(self):
+
+        return self.state
+
+    def getVehicleState(self):
+
+        return self.state
+    
+    def reset(self):
+
+        newState = States.vehicleState()
+        self.state = newState
+    
+    def setVehicleDot(self, dot):
+
+        dot = self.dot
+
+    def setVehicleState(self, state):
+
+        state = self.state
+        
