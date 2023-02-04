5307a6e5fd3710c78101f8a5e87ad35e374b4bf9
Robert Gaines <regaines@ucsc.edu>
2023-02-04 10:33:16 -0800

    changed drag coefficent and added skeleton code about to grind today.

--- ece163/Modeling/VehicleAerodynamicsModel.py
+++ ece163/Modeling/VehicleAerodynamicsModel.py
@@ -6,3 +6,50 @@ from ..Modeling import WindModel
 from ..Utilities import MatrixMath
 from ..Utilities import Rotations
 from ..Constants import VehiclePhysicalConstants as VPC
+
+
+class VehicleAerodynamicsModel:
+    
+    def __init__(self, initialSpeed = VPC.InitialSpeed, initialHeight = VPC.InitialDownPosition):
+        #create class instance of vehicle dynamics
+        self.dynamicsModel = VehicleDynamicsModel()
+        self.state = self.dynamicsModel.getVehicleState()
+        self.dot = self.dynamicsModel.getVehicleDerivative()
+        self.dT = self.dynamicsModel.dT
+
+        self.initialHeight = initialHeight
+        self.initialSpeed = initialSpeed
+
+
+    def reset(self):
+        pass
+    
+    def getVehicleState(self):
+        pass
+
+    def setVehicleState(self):
+        pass
+
+    def getVehicleDynamicsModel(self):
+        pass
+
+    def update(self):
+        pass
+
+    def CalculateCoeff_alpha(self):
+        pass
+
+    def aeroForces(self):
+        pass
+
+    def controlForces(self):
+        pass
+
+    def gravityForces(self):
+        pass
+
+    def CalculatePropForces(self):
+        pass
+
+    def updateForces(self):
+        pass
