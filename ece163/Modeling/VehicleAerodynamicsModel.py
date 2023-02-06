0b5d07cbba241d9467627e80c571658798b57984
Robert Gaines <regaines@ucsc.edu>
2023-02-06 11:40:41 -0800

    gravity fores passing now added calculateCoefAlpha

--- ece163/Modeling/VehicleAerodynamicsModel.py
+++ ece163/Modeling/VehicleAerodynamicsModel.py
@@ -1,3 +1,9 @@
+"""
+Engineer: Robert Gaines
+File Name : VehicleAerodynamicsModel.py
+Purpose : Contains functions to compute aerodynamic forces and moments acting on the MAV given certain control surface inputs
+"""
+
 import math
 from ..Containers import States
 from ..Containers import Inputs
@@ -6,7 +12,40 @@ from ..Modeling import WindModel
 from ..Utilities import MatrixMath
 from ..Utilities import Rotations
 from ..Constants import VehiclePhysicalConstants as VPC
+from matplotlib import pyplot as plt
+import numpy as np
 
+def cl_attached(alpha):
+    return VPC.Cl0 + VPC.CLalpha * alpha
+
+def cd_attached(alpha):
+    return VPC.CDp + (math.pow((VPC.CL0 + VPC.CLalpha * alpha),2))/(math.pi * math.e * VPC.AR)
+
+def cl_seperated(alpha):
+    return 2 *math.sin(alpha) * math.cos(alpha)
+
+def cd_seperated(alpha):
+    return 2 * math.pow(math.sin(alpha), 2)
+
+def Cl_fromA(alpha):
+                                #not sure what we should pass for M, do i need to calculate this?
+    sigma = (alpha, VPC.alpha0, 0.5)
+
+    return  (1 - sigma) * cl_attached(alpha) + sigma*cd_seperated(alpha)
+
+def Cd_fromA(alpha):
+                                #not sure what we should pass for M, do i need to calculate this?
+    sigma = (alpha, VPC.alpha0, 0.5)
+
+    return  (1 - sigma) * cd_attached(alpha) + sigma*cl_seperated(alpha)
+
+
+#blending function
+
+def sigma(a, a0, M):
+    num = (1 + np.exp(-M*(a-a0)) + np.exp(M*(a+a0)))
+    den = (1 + np.exp(-M*(a-a0)))*(1 + np.exp(M*(a+a0)))
+    return num/den
 
 class VehicleAerodynamicsModel:
     
@@ -46,7 +85,13 @@ class VehicleAerodynamicsModel:
     #Coefficient of Lift, CL_alpha (unitless), Coefficient of Drag,
     #CD_alpha (unitless), Coefficoent of Moment, CM_alpha (unitless)
     def CalculateCoeff_alpha(self, alpha):
-        pass
+        Cd_alpha = Cd_fromA(alpha)  
+        Cl_alpha = Cl_fromA(alpha)
+        Cm_alpha = VPC.CM0 + VPC.CMalpha * alpha
+         
+        return Cd_alpha, Cl_alpha, Cm_alpha
+
+
 
     def aeroForces(self, state):
         pass
