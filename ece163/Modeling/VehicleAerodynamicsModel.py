926423a98c31c7c6ee1278aba093d01cd4360fc8
Robert Gaines <regaines@ucsc.edu>
2023-02-06 16:54:21 -0800

    adding fixes to aeroForces no more divide by zero error

--- ece163/Modeling/VehicleAerodynamicsModel.py
+++ ece163/Modeling/VehicleAerodynamicsModel.py
@@ -204,20 +204,20 @@ class VehicleAerodynamicsModel:
             n = calcMomentN(state)
             Fy = calcFy(state)
 
-        print("end aeroForces")
-        forcesnMoments.Fx = Fl
-        forcesnMoments.Fy = Fy
-        forcesnMoments.Fz = Fd
-        forcesnMoments.Mx = l
-        forcesnMoments.My = m
-        forcesnMoments.Mz = n
+            print("end aeroForces")
+            forcesnMoments.Fx = Fl
+            forcesnMoments.Fy = Fy
+            forcesnMoments.Fz = Fd
+            forcesnMoments.Mx = l
+            forcesnMoments.My = m
+            forcesnMoments.Mz = n
 
-        m_1 = [[-1*Fd],[-1*Fl]]
-        m_2 = [[math.cos(state.alpha), -1*math.sin(state.alpha)],[math.sin(state.alpha), math.cos(state.alpha)]]
-        m_3 = MatrixMath.multiply(m_2, m_1)
-        
-        forcesnMoments.Fx = m_3[0][0]
-        forcesnMoments.Fz = m_3[1][0]
+            m_1 = [[-1*Fd],[-1*Fl]]
+            m_2 = [[math.cos(state.alpha), -1*math.sin(state.alpha)],[math.sin(state.alpha), math.cos(state.alpha)]]
+            m_3 = MatrixMath.multiply(m_2, m_1)
+            
+            forcesnMoments.Fx = m_3[0][0]
+            forcesnMoments.Fz = m_3[1][0]
         return forcesnMoments
 
     def controlForces(self, state, controls):
