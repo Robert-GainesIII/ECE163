9d7e7a85c5c5872aeaeb7024940fe36fc94a347b
Robert Gaines <regaines@ucsc.edu>
2023-02-06 17:55:38 -0800

    adding fixes to aeroForces no more divide by zero error

--- ece163/Modeling/VehicleAerodynamicsModel.py
+++ ece163/Modeling/VehicleAerodynamicsModel.py
@@ -243,8 +243,11 @@ class VehicleAerodynamicsModel:
         m_2 = [[math.cos(state.alpha), -1*math.sin(state.alpha)],[math.sin(state.alpha), math.cos(state.alpha)]]
         m_3 = MatrixMath.multiply(m_2, m_1)
         
-        forcesnMoments.Fx = m_3[0][0] + Fprop[0]
-        forcesnMoments.Fz = m_3[1][0] + Fprop[1]
+        forcesnMoments.Fx = m_3[0][0] 
+        forcesnMoments.Fz = m_3[1][0]
+
+        forcesnMoments.Fx += Fprop[0]
+        forcesnMoments.Mx += Fprop[1]
         return forcesnMoments
 
     def gravityForces(self, state):
