bd1acbfd541cf3b4b27e169be5c0523a03351744
Robert Gaines <regaines@ucsc.edu>
2023-02-13 10:37:19 -0800

    switched symbols in my head when implenting fixing this issue now

--- ece163/Modeling/WindModel.py
+++ ece163/Modeling/WindModel.py
@@ -58,13 +58,13 @@ class WindModel():
             if debug: print("zero division at Lu, Lv, Lw term?")
 
             if drydenParameters.Lu == 0:    drydenParameters.Lu = 0.1
-            uExponentTerm = -1*(Va*dT/drydenParameters.Lu)
+            uExponentTerm = -1*(Va*dT)/drydenParameters.Lu
 
             if drydenParameters.Lv == 0:    drydenParameters.Lv = 0.1
-            vExponentTerm = -1*(Va*dT/drydenParameters.Lv)
+            vExponentTerm = -1*(Va*dT)/drydenParameters.Lv
 
             if drydenParameters.Lw == 0:    drydenParameters.Lw = 0.1
-            wExponentTerm = -1*(Va*dT/drydenParameters.Lw)
+            wExponentTerm = -1*(Va*dT)/drydenParameters.Lw)
             if debug: print("nah guess not.")
             #Discrete parameritized time equivalent dryden wind model equations in U axis
             
