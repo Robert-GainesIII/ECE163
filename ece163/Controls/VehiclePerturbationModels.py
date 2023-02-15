b5fa8ac903440168e6f6874de04624e84df022e8
Robert Gaines <regaines@ucsc.edu>
2023-02-14 22:51:28 -0800

    updates

--- ece163/Controls/VehiclePerturbationModels.py
+++ ece163/Controls/VehiclePerturbationModels.py
@@ -24,8 +24,8 @@ def CreateTransferFunction(trimState, trimInputs):
     mytf.a_theta1 = 0
     mytf.a_theta2 = 0
     mytf.a_theta3 = 0
-    mytf.a_V1 = (((VPC.rho*(trimState.Va)* VPC.S)/VPC.mass)*(-VPC.CD0 + (VPC.CDalpha*trimState.alpha) - (VPC.CDdeltaE*trimInputs.Elevator))) + (1/VPC.mass * dThrust_dVa(trimState.Va,trimInputs.Throttle, 0.01))
-    mytf.a_V2 = 0
+    mytf.a_V1 = (((VPC.rho*trimState.Va* VPC.S)/VPC.mass)*(-VPC.CD0 + (VPC.CDalpha*trimState.alpha) - (VPC.CDdeltaE*trimInputs.Elevator))) + (1/VPC.mass * dThrust_dVa(trimState.Va,trimInputs.Throttle, 0.01))
+    mytf.a_V2 = 1/VPC.mass * dThrust_dThrottle(trimState.Va, trimInputs.Elevator)
     mytf.a_V3 = VPC.g0 * math.cos(trimState.pitch - trimState.alpha)
 
     return mytf
