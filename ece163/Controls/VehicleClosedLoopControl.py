8382f1f9f47940f9179f7d8d5869d4097930e99b
Robert Gaines <regaines@ucsc.edu>
2023-02-25 14:23:57 -0800

    updates to updates

--- ece163/Controls/VehicleClosedLoopControl.py
+++ ece163/Controls/VehicleClosedLoopControl.py
@@ -75,7 +75,7 @@ class PIControl():
 
         self.accumulator += (0.5 * self.dT) * (error + self.err)
 
-        derivative = (2*math.tau -self.dT)/(2*math.tau + self.dT) * derivative + (2/(2*math.tau + self.dT)) *  (error - self.err)
+        #derivative = (2*math.tau -self.dT)/(2*math.tau + self.dT) * derivative + (2/(2*math.tau + self.dT)) *  (error - self.err)
         
         u_u= (self.kp * error) + (self.ki * self.accumulator)  + self.trim
         u = u_u
