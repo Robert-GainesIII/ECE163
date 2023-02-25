d4bfc6afc9a03e3a2bf70372b66460fb9677d7cd
Robert Gaines <regaines@ucsc.edu>
2023-02-25 15:36:35 -0800

    updates to updates

--- ece163/Controls/VehicleControlGains.py
+++ ece163/Controls/VehicleControlGains.py
@@ -9,6 +9,8 @@ from ece163.Containers import Linearized
 from ece163.Utilities import MatrixMath
 from ece163.Utilities import Rotations
 
+
+
 def computeGains(tuningParameters, linearizedModel):
     controlGains = Controls.controlGains()
     
@@ -17,7 +19,21 @@ def computeGains(tuningParameters, linearizedModel):
 def computeTuningParameters(controlGains, linearizedModel):
 
     tuningParams = Controls.controlTuning()
-
+    tuningParams.Wn_roll  = 0
+    tuningParams.Zeta_roll  = 0
+    tuningParams.Wn_course  = 0	# Wn_roll should be 5-10x larger
+    tuningParams.Zeta_course  = 0 
+    tuningParams.Wn_sideslip  = 0
+    tuningParams.Zeta_sideslip  = 0
+    #tuning knobs for longitudinal control
+    tuningParams.Wn_pitch  = 0
+    tuningParams.Zeta_pitch  = 0
+    tuningParams.Wn_altitude  = 0	# Wn_pitch should be 5-10x larger
+    tuningParams.Zeta_altitude  = 0 
+    tuningParams.Wn_SpeedfromThrottle  = 0
+    tuningParams.Zeta_SpeedfromThrottle  = 0
+    tuningParams.Wn_SpeedfromElevator  = 0
+    tuningParams.Zeta_SpeedfromElevator  = 0
     return tuningParams
     
 
